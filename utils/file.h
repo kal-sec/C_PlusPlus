176c0922b7de6031e6264baa4ade8ce27846af95
Hélder Nogueira <git@linuxbytes.org>
2022-08-18 00:05:08 +0100

    test commit #2

--- utils/file.h
+++ utils/file.h
@@ -1,3 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <vector>
+
+class File
+{
+    private:
+        std::string fileName = std::string();
+        std::string filePath = std::string();
+
+    public:
+        const short unsigned init(std::string _path)
+        {
+            
+        }
+};
