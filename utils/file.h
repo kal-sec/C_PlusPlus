b62fe55c379cc949d5342fdf813745097a58e93e
Hélder Nogueira <git@linuxbytes.org>
2022-08-18 11:41:22 +0100

    test commit #4

--- utils/file.h
+++ utils/file.h
@@ -6,13 +6,23 @@
 class File
 {
     private:
+        std::ifstream inputStream = std::ifstream();
+        std::ofstream outputStream = std::ofstream();
+
         std::string fileName = std::string();
         std::string filePath = std::string();
 
     public:
         const short unsigned init(std::string _path)
         {
+            if(_path == "")
+                return 1;
             
+            outputStream.open(filePath);
+            outputStream << "";
+            outputStream.close();    
+
+            filePath = _path;
             return 0;
         }
 };
