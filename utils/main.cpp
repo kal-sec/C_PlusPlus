098d5ef7f5279433a9b172f20e97c061c4c25adb
Hélder Nogueira <git@linuxbytes.org>
2022-08-18 11:41:41 +0100

    test commit #5

--- utils/main.cpp
+++ utils/main.cpp
@@ -3,6 +3,10 @@
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    File f;
+    const int result = f.init("example.txt");
+
+    std::cout << "***** INIT RESULT *****" << std::endl;
+    std::cout << "VALUE: " << result << std::endl;
     return 0;
 }
